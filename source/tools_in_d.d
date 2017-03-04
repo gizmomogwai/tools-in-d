7d4681e6596ab8382b1825166c80894e5068bc82
Christian Köstlin <christian.koestlin@esrlabs.com>
2017-03-04 15:43:40 +0100

    added ability to have secondary names for one command of the multibinary
    e.g. dime is the main command and name and is used for importing and
    finding the correct dlang function, time is an alias and executes the
    same code

--- source/tools_in_d.d
+++ source/tools_in_d.d
@@ -1,8 +1,10 @@
 import std.path;
 import std.stdio;
 import std.algorithm.iteration;
+import std.algorithm.searching;
 import std.range;
 import std.format;
+import std.array;
 
 unittest {
   import unit_threaded;
@@ -10,39 +12,39 @@ unittest {
 }
 
 abstract class Executable {
-  public string name;
-  public this(string name) {
-    this.name = name;
+  public string[] names;
+  public this(string names) {
+    this.names = names.split(",");
   }
   abstract int run(string[] args);
 }
 
-Executable[] addExe(string name)(Executable[] executables=[]) {
+Executable[] addExe(string names)(Executable[] executables=[]) {
   mixin(
 `
-class %1$sExecutable : Executable {
+class %2$sExecutable : Executable {
   public this() {
     super("%1$s");
   }
   override int run(string[] args) {
-    import %1$s : %1$s;
-    return %1$s(args);
+    import %2$s : %2$s;
+    return %2$s(args);
   }
 }
-return executables ~ new %1$sExecutable();
-`.format(name));
+return executables ~ new %2$sExecutable();
+`.format(names, names.split(",")[0]));
 }
 
 int toolsInD(string[] args) {
   auto executables =
-    addExe!("dime").
+    addExe!("dime,time").
     addExe!("dir").
     addExe!("zen")
     ;
 
-  auto command = args[0].baseName;
+  auto commandName = args[0].baseName;
   foreach (exe; executables) {
-    if (exe.name == command) {
+    if (exe.names.canFind(commandName)) {
       return exe.run(args);
     }
   }
